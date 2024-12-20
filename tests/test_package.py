e372b299e75036c9b53b86051febada5cbfb447b
Matthew Trudeau <mtrudeau@foundrydigital.com>
2024-12-20 14:25:02 -0500

    Increment version to v0.2.0

--- tests/test_package.py
+++ tests/test_package.py
@@ -16,4 +16,4 @@ class TestPackage(unittest.TestCase):
     def test_package_version(self):
         # Check that version is as expected
         # Must update to increment package version successfully
-        self.assertEqual(__version__, "0.1.0")
+        self.assertEqual(__version__, "0.2.0")
