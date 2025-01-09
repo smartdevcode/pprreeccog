11a0f0695e8e4a66d51c156d5ab67c4311b68884
hscott <hscott@foundrydigital.com>
2025-01-09 10:38:40 -0500

    pm2 argument parsing fix

--- precog/validators/validator.py
+++ precog/validators/validator.py
@@ -2,13 +2,14 @@ import argparse
 import asyncio
 from pathlib import Path
 
-from precog.utils.config import config
+import bittensor as bt
+from precog.utils.config import config,add_args, add_validator_args
 from precog.validators.weight_setter import weight_setter
 
 
 class Validator:
-    def __init__(self):
-        self.config = config(parser=argparse.ArgumentParser(), neuron_type="validator")
+    def __init__(self, config):
+        self.config = config
         print(self.config)
         full_path = Path(
             f"{self.config.logging.logging_dir}/{self.config.wallet.name}/{self.config.wallet.hotkey}/netuid{self.config.netuid}/validator"
@@ -27,5 +28,9 @@ class Validator:
 
 # Run the validator.
 if __name__ == "__main__":
-    validator = Validator()
+    parser = argparse.ArgumentParser()
+    add_args(parser)
+    add_validator_args(parser)
+    config = bt.config(parser)
+    validator = Validator(config)
     asyncio.run(validator.main())
