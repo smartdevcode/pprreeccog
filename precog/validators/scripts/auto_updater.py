93c402f497ecf5b247b2fa9fed5722e3737a5437
Matt | Yuma | DCG <173096484+matt-yuma@users.noreply.github.com>
2025-03-24 11:18:22 -0400

    Auto Updater Scheduling (#104)
    * Attempt using built in pm2 cron
    
    * Change commit hash
    
    * Remove commented code
    
    * Run frequency is now controlled by cron

--- precog/validators/scripts/auto_updater.py
+++ precog/validators/scripts/auto_updater.py
@@ -1,16 +1,11 @@
 import importlib.resources as pkg_resources
 import subprocess
 import time
-from datetime import timedelta
 
 import bittensor as bt
 import git
 
 import precog
-from precog.utils.timestamp import elapsed_seconds, get_now
-
-# Frequency of the auto updater in minutes
-TIME_INTERVAL = 5
 
 
 def git_pull_change(path, max_retries=3, retry_delay=5) -> bool:
@@ -58,50 +53,21 @@ if __name__ == "__main__":
     with pkg_resources.path(precog, "..") as p:
         git_repo_path = p
 
-    # Loop until we observe github activity
-    while True:
+    bt.logging.debug("Checking for repository changes...")
 
-        # Get current timestamp
-        now = get_now()
+    # Pull the latest changes from github
+    has_changed = git_pull_change(git_repo_path)
 
-        # Check if the current minute is 2 minutes past anticipated validator query time
-        if now.minute % TIME_INTERVAL == 2:
+    # If the repo has not changed
+    if not has_changed:
+        bt.logging.debug("Repository has not changed. Sleep mode activated.")
 
-            bt.logging.debug("Checking for repository changes...")
+    # If the repository has changed
+    else:
+        bt.logging.debug("Repository has changed!")
 
-            # Pull the latest changes from github
-            has_changed = git_pull_change(git_repo_path)
-
-            # If the repo has changed, break the loop
-            if has_changed:
-                bt.logging.debug("Repository has changed!")
-                break
-
-            # If the repo has not changed, sleep
-            else:
-                bt.logging.debug("Repository has not changed. Sleep mode activated.")
-
-                # Calculate the time of the next git pull check
-                next_check = now + timedelta(minutes=TIME_INTERVAL)
-                next_check = next_check.replace(second=0)
-
-                # Determine the number of seconds to sleep
-                seconds_to_sleep = elapsed_seconds(get_now(), next_check)
-
-                # Sleep for the exact number of seconds to the next git pull check
-                time.sleep(seconds_to_sleep)
-        else:
-
-            # Sleep for 45 seconds
-            # This is to prevent the script from checking for changes too frequently
-            # This specific `else` block should not be reach too often since we sleep for the exact time of the anticipated validator query time
-            bt.logging.debug("Sleeping for 45 seconds")
-            time.sleep(45)
-
-    # This code is only reached when the repo has changed
-    # We can now restart both pm2 processes, including the auto updater
-    # Let the script simply end and the new process will be restarted by pm2
-    bt.logging.debug("Installing dependencies...")
-    subprocess.run(["poetry", "install"], cwd=git_repo_path)
-    bt.logging.debug("Restarting pm2 processes...")
-    subprocess.run(["pm2", "restart", "app.config.js"], cwd=git_repo_path)
+        # We can now restart both pm2 processes, including the auto updater
+        bt.logging.debug("Installing dependencies...")
+        subprocess.run(["poetry", "install"], cwd=git_repo_path)
+        bt.logging.debug("Restarting pm2 processes...")
+        subprocess.run(["pm2", "restart", "app.config.js"], cwd=git_repo_path)
