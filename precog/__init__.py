b3f6658a39ffe2b420349686dc32d6a57ab9527c
Adonistoday <lovevenus1015@gmail.com>
2025-10-24 07:29:53 -0400

    v1.1.3

--- precog/__init__.py
+++ precog/__init__.py
@@ -1,5 +1,10 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version(__name__ or __package__)
+try:
+    __version__ = importlib.metadata.version(__name__ or __package__)
+except importlib.metadata.PackageNotFoundError:
+    # Fallback for local development
+    __version__ = "3.0.0"
+
 version_split = __version__.split(".")
 __spec_version__ = (1000 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
